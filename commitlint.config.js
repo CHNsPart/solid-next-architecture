7451690ccd6a65650630abd625583716e0d89722
chnspart <imchn24@gmail.com>
2023-01-30 13:09:55 +0600

    ci: project structure

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,32 @@
 module.exports = {
-    extends: [
-        '@commitlint/config-conventional'
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [2, 'always', 100],
+    'body-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 100],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
     ],
-    rules: {
-        "type-enum": [
-            2, 
-            "always", [
-                "build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test", "button"
-            ]
-        ],
-    }
-}
+    'scope-case': [2, 'always', 'lower-case'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'button',
+      ],
+    ],
+  },
+};
