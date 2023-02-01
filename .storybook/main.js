f6051208edb333d6aea40795c81a49998d35ef88
chnspart <imchn24@gmail.com>
2023-02-01 17:14:38 +0600

    ci: storybook added

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,31 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../**/*stories/**/*.stories.mdx',
+    '../**/*stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+  staticDir: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack"
-  }
-}
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+};
+// module.exports = {
+//   "stories": [
+//     "../stories/**/*.stories.mdx",
+//     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+//   ],
+//   "addons": [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials",
+//     "@storybook/addon-interactions"
+//   ],
+//   "framework": "@storybook/react",
+//   "core": {
+//     "builder": "@storybook/builder-webpack5"
+//   }
+// }
